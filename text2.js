a2f8aceafb7ea0929c5e805f33a1e2cb112d0838
nguyen dinh long khanh <longgkhanh9@gmail.com>
2023-06-05 10:47:36 +0700

    ThiTH

--- text2.js
+++ text2.js
@@ -1,15 +1,15 @@
-function timViTriPhanTu(element, array) {
-    for (let i = 0; i < element.length; i++) {
-        if (element[i] == array) {
+function timViTriPhanTu(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == element) {
             return i;
         }
     }
     return -1;
 }
 
-let element = [1, 2, 3, 4, 6, 7, 5, 9, 10];
-let array = prompt('Nhập giá Trị: ');
-let index = timViTriPhanTu(element, array);
+let array = [1, 2, 3, 4, 6, 7, 5, 9, 10];
+let element = prompt('Nhập giá Trị: ');
+let index = timViTriPhanTu(array, element);
 
 if (index == -1) {
     alert('không có phần tử trong mảng');
